9071d1b1d64bc701c758d9e120e9dea689a8c684
samithw <mailtosamith@gmail.com>
2022-09-20 12:47:49 +0530

    bg color changed for icons in contact

--- src/pages/contact/index.page.tsx
+++ src/pages/contact/index.page.tsx
@@ -65,7 +65,7 @@ function Page() {
 
           
           
-          <ContentSeparater bgCSS = {{ backgroundColor : "#0b7dc4" }}>
+          <ContentSeparater bgCSS = {{ backgroundColor : "#3F5157" }}>
               {{
                 imageURLs : [
                   "/images/icon__contact__1.png",
