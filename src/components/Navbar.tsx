4ea1af88e29ab682f528e0acacf013635827c3a7
samithw <mailtosamith@gmail.com>
2022-10-27 18:47:34 +0530

    updated nav links to have ending slash [SEO}

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,29 +44,29 @@ const Navbar = (props: Props) => {
           <Link href="/"><span>home</span></Link>
         </li>
         <li className="nav-item">
-          <Link href="/about"><span>about</span></Link>
+          <Link href="/about/"><span>about</span></Link>
         </li>
         {/* <li className="nav-item">
           <a href="#"><span>products</span></a>
         </li> */}
         <li className="nav-item has-sub-menu">
-          <Link href="/services"><span>services</span></Link>
+          <Link href="/services/"><span>services</span></Link>
           <ul className='md:absolute p-0 m-0 md:hidden border-2 border-teal-600 bg-[#282828] bg-opacity-70 rounded-b-xl md:rounded-tr-xl '>
-            <li className=' sub-nav'><Link href="/services/business-matchmaking"><span>business matchmaking</span></Link></li>
-            <li className=' sub-nav'><Link href="/services/direct-market-entrance"><span>direct market entrance</span></Link></li>
-            <li className=' sub-nav'><Link href="/services/direct-touch-sales-activities"><span>direct touch sales activities</span></Link></li>
-            <li className=' sub-nav'><Link href="/services/inbound-outbound-marketing"><span>inbound and outbound marketing</span></Link></li>
-            <li className=' sub-nav'><Link href="/services/channel-development-channel-management"><span>channel development and channel management</span></Link></li>
-            <li className=' sub-nav'><Link href="/services/sales-outsourcing"><span>sales outsourcing</span></Link></li>
-            <li className=' sub-nav'><Link href="/services/gateway-to-indian-market"><span>gateway to indian market</span></Link></li>
-            <li className=' sub-nav'><Link href="/services/building-information-modeling"><span>building information modeling</span></Link></li>
+            <li className=' sub-nav'><Link href="/services/business-matchmaking/"><span>business matchmaking</span></Link></li>
+            <li className=' sub-nav'><Link href="/services/direct-market-entrance/"><span>direct market entrance</span></Link></li>
+            <li className=' sub-nav'><Link href="/services/direct-touch-sales-activities/"><span>direct touch sales activities</span></Link></li>
+            <li className=' sub-nav'><Link href="/services/inbound-outbound-marketing/"><span>inbound and outbound marketing</span></Link></li>
+            <li className=' sub-nav'><Link href="/services/channel-development-channel-management/"><span>channel development and channel management</span></Link></li>
+            <li className=' sub-nav'><Link href="/services/sales-outsourcing/"><span>sales outsourcing</span></Link></li>
+            <li className=' sub-nav'><Link href="/services/gateway-to-indian-market/"><span>gateway to indian market</span></Link></li>
+            <li className=' sub-nav'><Link href="/services/building-information-modeling/"><span>building information modeling</span></Link></li>
           </ul>
         </li>
         <li className="nav-item">
-          <Link href="/technology"><span>technology</span></Link>
+          <Link href="/technology/"><span>technology</span></Link>
         </li>
         <li className="nav-item">
-          <Link href="/contact"><span>contact</span></Link>
+          <Link href="/contact/"><span>contact</span></Link>
         </li>
       </ul>
       <button onClick={handleCLick} className="md:hidden z-100" id="mobileNavBtn">
