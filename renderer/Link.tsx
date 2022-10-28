3577e73c0f83e7b70d0bf9dffda22583b268aed0
samithw <mailtosamith@gmail.com>
2022-10-29 00:41:38 +0530

    active link trailing slash removal

--- renderer/Link.tsx
+++ renderer/Link.tsx
@@ -5,7 +5,12 @@ export { Link }
 
 function Link(props: { href: string; className?: string; children: React.ReactNode }) {
   const pageContext = usePageContext()
-  const isActive = (props.href != "/" && pageContext.urlPathname.includes(props.href)) || pageContext.urlPathname === props.href
+  const linkHref = removeTrailingSlash(props.href)
+  const isActive = (props.href != "/" && pageContext.urlPathname.includes(linkHref)) || pageContext.urlPathname === props.href
   const className = [props.className, isActive && 'active'].filter(Boolean).join(' ')
   return <a {...props} className={className} />
+}
+
+function removeTrailingSlash(str: string) {
+  return str.replace(/\/+$/, '');
 }
